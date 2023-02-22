6777ccd62c2c9bdde874b47f8d1697a6c166b106
gleidson fagno <gleidsonfagner838@gmail.com>
2023-02-22 10:11:38 -0300

    portifolio

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function  OpenMenu(){
+     document.getElementById("menu").style.width = "100%"
+     // alert("certo")
+}
+
+function CloseMenu(){
+      document.getElementById("menu").style.width = "0"
+     //  alert("certo")
+}
